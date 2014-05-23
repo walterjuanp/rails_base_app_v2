8beb2ef8fe334412c0cfc5b4bcee539b41ebf11c
Walter Juan <walterjuanp@gmail.com>
2014-05-23 23:59:37 +0200

    Overwrite after_sign_in_path_for, now after login an Admin user this will be redirect always to admin_path or stored location

--- site/app/controllers/application_controller.rb
+++ site/app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  #
+  # Overwrite method from devise
+  #
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      stored_location_for(resource) || admin_path
+    else
+      super
+    end
+  end
 end
