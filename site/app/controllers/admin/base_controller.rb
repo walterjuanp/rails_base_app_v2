2138d696c54cd51cfcb832ebaa780fa13b5c1ca6
Walter Juan <walterjuanp@gmail.com>
2014-05-23 23:46:16 +0200

    Controll access for 'admin' namespaces, only can access the admin users

--- site/app/controllers/admin/base_controller.rb
+++ site/app/controllers/admin/base_controller.rb
@@ -6,4 +6,7 @@ class Admin::BaseController < ApplicationController
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Only admin users can access
+  before_action :authenticate_admin!
+
 end
