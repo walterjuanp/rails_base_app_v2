3b949277f6a608ff94d004014167ba0eb5701bf8
Walter Juan <walterjuanp@gmail.com>
2014-05-25 16:18:00 +0200

    Move all devise generated files inside 'admin' module. This means, add options for devise_for, add custom controllers, move views inside admin and remove unused views.

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, :module => 'admin', :path => 'admin'
   root 'home#index'
 
   #######################################################
